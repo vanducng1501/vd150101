42cba9f283f74e03c52d883703c9701c85446f8c
Van Duc Nguyen <vanducng@vanducng-Inspiron-5521>
2022-05-05 20:29:44 +0700

    practice C++ 29/04/2022

---
+++ exception.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std; 
+
+int main ()
+{
+    try 
+    {
+        int motherAge = 29;
+        int sonAge = 34;
+        if (sonAge > motherAge)
+        { throw 99;}
+    }
+    catch (int x)
+    { cout << "Wrong age values - Error" << x;}
+
+    return 0;
+}
